d67660ffd581e79945ea6712845432515b83a8e4
endiy0 <steve060401@gmail.com>
2025-03-29 16:16:59 +0900

    v0.0.1-a
    Initial commit

---
+++ Server test/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Server_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            listBox2 = new ListBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(588, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 32);
+            label1.TabIndex = 0;
+            label1.Text = "포트";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(588, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(794, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 46);
+            button1.TabIndex = 2;
+            button1.Text = "서버 시작";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(794, 92);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 46);
+            button2.TabIndex = 3;
+            button2.Text = "서버 종료";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(570, 612);
+            listBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 634);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(414, 39);
+            textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(432, 630);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 6;
+            button3.Text = "전송";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(588, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(141, 128);
+            label2.TabIndex = 7;
+            label2.Text = "로컬 IP주소:\r\n0.0.0.0\r\n외부 IP주소:\r\n0.0.0.0";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listBox2);
+            groupBox1.Location = new Point(594, 230);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(400, 446);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "접속자";
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(6, 38);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(388, 388);
+            listBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1006, 711);
+            Controls.Add(groupBox1);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Chat Server";
+            FormClosing += Form1_FormClosing;
+            KeyDown += Form1_KeyDown;
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private Label label2;
+        private GroupBox groupBox1;
+        private ListBox listBox2;
+    }
+}
---
+++ Server test/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_test
+{
+    public partial class Form1 : Form
+    {
+        static TcpListener server;
+        static List<Client> clients;
+        Thread T;
+        List<Thread> Tt;
+        static bool isServerRun;
+        static bool isClosing;
+        public Form1()
+        {
+            InitializeComponent();
+            clients = new List<Client>();
+            isServerRun = false;
+            T = new Thread(() => ServerLoop(1111));
+            Tt = new List<Thread>();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            isClosing = false;
+            label2.Text = "로컬 IP주소:\n" + GetLocalIPAddress() + "\n외부 IP주소:\n" + GetExternalIPAddress();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int port) && 0 < port && port < 100000)
+            {
+
+                T = new Thread(() => ServerLoop(port));
+                T.IsBackground = true;
+                T.Start();
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                isServerRun = true;
+                listBox1.Items.Add("Server started");
+            }
+            else
+            {
+                MessageBox.Show("포트는 1에서 99999 사이의 정수를 입력해 주세요");
+            }
+        }
+        /*
+        입력 코드
+        0:채팅
+        1:연결종료
+        2:번호 지정(서버=>클라이언트)
+        3:닉네임 전송(클라이언트=>서버)
+        4:접속한 클라이언트 이름
+        5:접속 종료한 클라이언트 이름
+         */
+        //Split 문자 : ⧫
+        //송신 Check 문자 : ◊
+
+        public void Delay(int ms)
+        {
+            DateTime dateTimeNow = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, ms);
+            DateTime dateTimeAdd = dateTimeNow.Add(duration);
+            while (dateTimeAdd >= dateTimeNow)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                dateTimeNow = DateTime.Now;
+            }
+            return;
+        }
+
+        //Thread func
+        void ServerLoop(int port)
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            isServerRun = true;
+            
+            int count = 0;
+            byte[] buffer;
+
+            while (true)
+            {
+                try
+                {
+                    clients.Add(new Client(server.AcceptTcpClient(), count));
+                    Invoke(new Action(() => listBox2.Items.Add(clients[clients.Count - 1].nickname)));
+                    count++;
+                    
+                    Tt.Add(new Thread(() => ClientCheck(clients.Count - 1, count)));
+                    Delay(100);
+                    clients[clients.Count - 1].client.GetStream().Write(Encoding.UTF8.GetBytes($"2⧫{count}◊"));
+                    Tt[Tt.Count - 1].IsBackground = true;
+                    Tt[Tt.Count - 1].Start();
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+            }
+        }
+
+        void ClientCheck(int clientrealnumber, int clientn)
+        {
+            Client client = clients[clientrealnumber];
+            NetworkStream stream = clients[clientrealnumber].client.GetStream();
+            byte[] buffer = new byte[102400];
+            buffer[102399] = 255;
+            bool error = false;
+            string msg = "";
+            while (isServerRun)
+            {
+                try
+                {
+
+                    buffer = new byte[102400];
+                    if(msg != "")
+                    {
+                        buffer = Encoding.UTF8.GetBytes(msg);
+                    }
+                    while (true)
+                    {
+                        byte[] data = new byte[256];
+                        int bytesRead = stream.Read(data, 0, data.Length);
+                        if (bytesRead == 0)
+                            break;
+                        data = data.Where(x => x != 0).ToArray();
+                        if (buffer.Length == 102400) buffer = data;
+                        else buffer = buffer.Concat(data).ToArray();
+
+                        msg = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                        if (msg.Contains('◊')) break;
+                    }
+                    if (Encoding.UTF8.GetString(buffer, 0, buffer.Length).Split("◊").Length == 1)
+                        msg = "";
+                    else msg = Encoding.UTF8.GetString(buffer, 0, buffer.Length).Split("◊")[1];
+                    string[] message = Encoding.UTF8.GetString(buffer, 0, buffer.Length).Split("◊")[0].Split('⧫');
+                    if (message[0] == "0")
+                    {
+                        Invoke(new Action(() => listBox1.Items.Add(message[1])));
+
+                        foreach (var c in clients)
+                        {
+                            if (c != client)
+                            {
+                                NetworkStream cStream = c.client.GetStream();
+                                byte[] responseBytes = Encoding.UTF8.GetBytes("0⧫" + message[1] + '◊');
+                                cStream.Write(responseBytes, 0, responseBytes.Length);
+                            }
+                        }
+                    }
+                    else if (message[0] == "1")
+                    {
+                        
+                        
+                        Invoke(new Action(() => listBox1.Items.Add($"{client.nickname} disconnected...")));
+                        Invoke(new Action(() => listBox2.Items.Remove(client.nickname)));
+                        foreach (var c in clients)
+                        {
+                            NetworkStream cStream = c.client.GetStream();
+                            byte[] responseBytes = buffer;
+                            if (c != client)
+                            {
+                                cStream.Write(Encoding.UTF8.GetBytes($"0⧫{client.nickname} disconnected...◊"));
+                                cStream.Flush();
+                                Delay(100);
+                                cStream.Write(Encoding.UTF8.GetBytes($"5⧫{client.nickname}◊"));
+                                cStream.Flush();
+                            }
+                            
+                        }
+                        break;
+                    }
+                    else if (message[0] == "3")
+                    {
+                        foreach (var c in clients)
+                        {
+                            if(c.nickname == message[1])
+                            {
+                                string nickname = "";
+                                foreach (var c2 in clients)
+                                {
+                                    if(c2 != client) nickname += c2.nickname + ", ";
+                                }
+                                client.client.GetStream().Write(Encoding.UTF8.GetBytes("1⧫닉네임은 다음과 같을 수 없습니다:"+ nickname+ '◊'));
+                                clients.Remove(client);
+                                Invoke(new Action(() => listBox2.Items.Remove(client.nickname)));
+                                int b = 0;
+                                error = true;
+                                int a = 10 / b;
+                            }
+                        }
+                        clients.Remove(client);
+                        Invoke(new Action(() => listBox2.Items.Remove(client.nickname)));
+                        client.nickname = message[1];
+                        foreach(var c in clients)
+                        {
+                            client.client.GetStream().Write(Encoding.UTF8.GetBytes("4⧫" + c.nickname + '◊'));
+                            client.client.GetStream().Flush();
+                            Delay(100);
+                        }
+                        clients.Add(client);
+                        foreach(var c in clients)
+                        {
+                            c.client.GetStream().Write(Encoding.UTF8.GetBytes("4⧫" + client.nickname + '◊'));
+                        }
+                        Invoke(new Action(() => listBox2.Items.Add(client.nickname)));
+                        Invoke(new Action(() => listBox1.Items.Add($"{message[1]} joined")));
+                        buffer = Encoding.UTF8.GetBytes($"0⧫{client.nickname} joined◊");
+                        foreach (var c in clients)
+                        {
+                            NetworkStream s = c.client.GetStream();
+                            s.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+                    Invoke(new Action(() => listBox1.TopIndex = listBox1.Items.Count - 1));
+                }
+                catch (Exception e)
+                {
+
+                    
+                    break;
+                }
+            }
+            //if (!isClosing && !error)
+            //{
+            //    foreach (var c in clients)
+            //    {
+            //        if (c != clients[clientrealnumber])
+            //        {
+            //            NetworkStream cStream = c.client.GetStream();
+            //            byte[] responseBytes = buffer;
+            //            cStream.Write(Encoding.UTF8.GetBytes($"{clients[clientrealnumber].nickname} disconnected..."));
+            //        }
+            //    }
+            //    Invoke(new Action(() => listBox1.Items.Add($"{clients[clientrealnumber].nickname} disconnected...")));
+            //}
+            client.client.Close();
+            if (!isClosing)
+            {
+                Invoke(new Action(() => listBox1.Items.Remove(client.nickname)));
+                clients.Remove(client);
+            }
+        }
+        //TODO: 현재 접속자명 listbox 만들기
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isClosing = true;
+            foreach (var c in clients)
+            {
+                NetworkStream n = c.client.GetStream();
+                n.Write(Encoding.UTF8.GetBytes("1⧫◊"));
+                c.client.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var c in clients)
+            {
+                c.client.GetStream().Write(Encoding.UTF8.GetBytes("1⧫◊"));
+                c.client.Close();
+            }
+            button2.Enabled = false;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            isServerRun = false;
+            listBox1.Items.Add("Server stopped");
+            server.Stop();
+            listBox2.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains('⧫') && !textBox1.Text.Contains('◊'))
+            {
+                if (textBox1.Text != "")
+                {
+                    foreach (var c in clients)
+                    {
+                        c.client.GetStream().Write(Encoding.UTF8.GetBytes("0⧫" + "Server:" + textBox2.Text + '◊'));
+                    }
+                    listBox1.Items.Add("Server:" + textBox2.Text);
+                    textBox2.Text = "";
+                    listBox1.TopIndex = listBox1.Items.Count - 1;
+                }
+                else
+                {
+                    MessageBox.Show("문자는 공백이면 안됩니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("채팅에 다음 문자는 포함되면 안됩니다: ⧫, ◊");
+            }
+        }
+
+        static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("로컬 IP 주소를 찾을 수 없습니다.");
+        }
+
+        static string GetExternalIPAddress()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string response = client.DownloadString("https://api.ipify.org");
+                return response;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && isServerRun)
+            {
+                if (!textBox1.Text.Contains('⧫') && !textBox1.Text.Contains('◊'))
+                {
+                    if (textBox1.Text != "")
+                    {
+                        foreach (var c in clients)
+                        {
+                            c.client.GetStream().Write(Encoding.UTF8.GetBytes("0⧫" + "Server:" + textBox2.Text + '◊'));
+                        }
+                        listBox1.Items.Add("Server:" + textBox2.Text);
+                        textBox2.Text = "";
+                        listBox1.TopIndex = listBox1.Items.Count - 1;
+                    }
+                    else
+                    {
+                        MessageBox.Show("문자는 공백이면 안됩니다.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("채팅에 다음 문자는 포함되면 안됩니다: ⧫, ◊");
+                }
+            }
+        }
+    }
+    class Client
+    {
+        public TcpClient client;
+        public string nickname;
+
+        public Client(TcpClient client, int n)
+        {
+            this.client = client;
+            nickname = "Client" + n.ToString();
+        }
+        public Client(TcpClient client, string str)
+        {
+            this.client = client;
+            nickname = str;
+        }
+    }
+}
