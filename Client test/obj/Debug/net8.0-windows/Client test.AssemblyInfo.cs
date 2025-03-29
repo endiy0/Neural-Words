e941466a2eabc15c05441b243b2dd992ae90d7fe
endiy0 <steve060401@gmail.com>
2025-03-29 20:33:05 +0900

    v0.0.1-c
    Using model change(FastTextWrapper)

--- Client test/obj/Debug/net8.0-windows/Client test.AssemblyInfo.cs
+++ Client test/obj/Debug/net8.0-windows/Client test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67660ffd581e79945ea6712845432515b83a8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f538f0e1367e765b919734bd0ed8d2176872c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
